84e9358e0a7e49f4be26df7d9efcbd2081dce080
Senait <senutekie77@gmail.com>
2021-02-26 08:23:41 -0800

    add data persistence

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -237,5 +237,24 @@ var saveTasks = function() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
+var loadTasks = function(){
+
+    var savedTasks = localStorage.getItem("tasks");
+
+  if (!savedTasks) {
+    return false;
+  }
+
+  savedTasks = JSON.parse(savedTasks);
+
+   // loop through savedTasks array
+for (var i = 0; i < savedTasks.length; i++) {
+    // pass each task object into the `createTaskEl()` function
+    createTaskEl(savedTasks[i]);
+  }
+
+}
+loadTasks();
+
 pageContentEl.addEventListener("click", taskButtonHandler);
 pageContentEl.addEventListener("change", taskStatusChangeHandler);
